0b5a1c7a4e6a45cae6259ea0d23f3bc4c0cc2a56
Thorbjörn Heise <thorbjoern.heise@gmail.com>
2024-03-01 22:50:58 +0000

    Creat ClLient db model and app

---
+++ edubba/client/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Client(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=15)
+    comment = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name="clients", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def full_name(self):
+        return f"{self.firstname} {self.lastname}"
+
+    def __str__(self):
+        return self.full_name

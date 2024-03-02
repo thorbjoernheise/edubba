c3b8b8aea16b01eab14b922a2920a3a030f497e1
Thorbjörn Heise <thorbjoern.heise@gmail.com>
2024-03-02 00:30:09 +0000

    Created Client details, updated buttons

--- edubba/client/views.py
+++ edubba/client/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Client
@@ -10,3 +10,12 @@ def clients_list(request):
     return render(request, "client/clients_list.html", {
         "clients" : clients
     })
+
+@login_required
+def clients_detail(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    return render(request, "client/clients_detail.html", {
+    "client" : client
+})
+
